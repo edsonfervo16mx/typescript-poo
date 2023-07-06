3005f980e3b80d774a5e94abdcdbf80285a1680a
edsonfervo <edson55_@hotmail.com>
2023-07-06 13:38:14 -0500

    generics for class and base http service with interface

---
+++ src/app/services/base-http-service.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+import { Category } from "../models/category.model";
+import { Product } from "../models/product.model";
+
+export class BaseHttpService<TypeClass> {
+  constructor(private url: string) {}
+
+  async getAll() {
+    const { data } = await axios.get<TypeClass[]>(this.url);
+    return data;
+  }
+}
+
+(async () => {
+  const url1 = "https://api.escuelajs.co/api/v1/products";
+  const productService = new BaseHttpService<Product>(url1);
+
+  const rta = await productService.getAll();
+  console.log("products", rta.length);
+
+  const url2 = "https://api.escuelajs.co/api/v1/categories";
+  const categoryService = new BaseHttpService<Category>(url2);
+
+  const rta1 = await categoryService.getAll();
+  console.log("categories", rta1.length);
+})();
